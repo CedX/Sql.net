3fde6e53a6f850dfef1f3c07168fd72d26ecef65
Cédric Belin <cedx@outlook.com>
2026-06-27 15:44:45 +0200

    Rename a parameter

--- Sources/Command/SqlParameterCollection.cs
+++ Sources/Command/SqlParameterCollection.cs
@@ -53,8 +53,8 @@ public class SqlParameterCollection(params IEnumerable<SqlParameter> parameters)
 	/// </summary>
 	/// <param name="parameters">The hash table whose elements are copied to the parameter collection.</param>
 	/// <returns>The parameter collection corresponding to the specified hash table of named parameters.</returns>
-	public static implicit operator SqlParameterCollection(Hashtable hashtable) =>
-		hashtable.Cast<DictionaryEntry>().ToDictionary(entry => entry.Key.ToString() ?? "", entry => entry.Value);
+	public static implicit operator SqlParameterCollection(Hashtable parameters) =>
+		parameters.Cast<DictionaryEntry>().ToDictionary(entry => entry.Key.ToString() ?? "", entry => entry.Value);
 
 	/// <summary>
 	/// Adds a new positional parameter to the end of this collection.
