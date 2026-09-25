2bf4a3c474cd64c084815d660d1f68476d2f0a1e
Cédric Belin <cedx@outlook.com>
2026-09-25 10:53:37 +0200

    Fix the documentaton

--- Sources/Object/DbConnection.Delete.cs
+++ Sources/Object/DbConnection.Delete.cs
@@ -49,19 +49,17 @@ public static partial class DbConnectionExtensions {
 		/// <param name="timeout">The wait time, in seconds, before terminating the attempt to execute the command and generating an error.</param>
 		/// <param name="transaction">The transaction within which the command executes.</param>
 		/// <param name="builder">An optional command builder used to build the SQL query to be executed.</param>
-		/// <returns><see langword="true"/> if the specified entity has been deleted, otherwise <see langword="false"/>.</returns>
 		public void DeleteAll<T>(bool truncate = false, int timeout = 30, IDbTransaction? transaction = null, SqlCommandBuilder? builder = null) where T: new() =>
 			connection.DeleteAll(typeof(T), truncate, timeout, transaction, builder);
 
 		/// <summary>
 		/// Deletes all entities.
 		/// </summary>
-		/// <param name="type">The entity type.</type>
+		/// <param name="type">The entity type.</param>
 		/// <param name="truncate">Value indicating whether to truncate the underlying table.</param>
 		/// <param name="timeout">The wait time, in seconds, before terminating the attempt to execute the command and generating an error.</param>
 		/// <param name="transaction">The transaction within which the command executes.</param>
 		/// <param name="builder">An optional command builder used to build the SQL query to be executed.</param>
-		/// <returns><see langword="true"/> if the specified entity has been deleted, otherwise <see langword="false"/>.</returns>
 		public void DeleteAll(Type type, bool truncate = false, int timeout = 30, IDbTransaction? transaction = null, SqlCommandBuilder? builder = null) {
 			var (command, parameters) = (builder ?? SqlCommandBuilder.Create(connection)).GetDeleteAllCommand(type, truncate);
 			command.Timeout = timeout;
